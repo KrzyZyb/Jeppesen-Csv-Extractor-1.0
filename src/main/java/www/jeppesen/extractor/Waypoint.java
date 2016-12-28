528cfc04b3e12461854f884a9ce3ae6383f800eb
KrzyZyb <krzysztof.zybul@gmail.com>
2016-12-28 21:10:15 +0100

    *Adding waypoint class comments*

--- src/main/java/www/jeppesen/extractor/Waypoint.java
+++ src/main/java/www/jeppesen/extractor/Waypoint.java
@@ -11,6 +11,17 @@ public class Waypoint {
 	private String latitude;
 	private String longitude;
 	private String longxlati;
+	
+	/*
+	 * Constructor for waypoints which country name is made of two parts (for example: Poland, republic of)
+	 * @param country - String with first part of country name
+	 * @param country2 - String with second part of coutry name
+	 * @param ICAO - Operators code String
+	 * @param WPT_id Particular Waypoint id, that is unique (can not be repeated in whole continent)
+	 * @param latitude - String with latitude data
+	 * @param longitude - String with longitude data
+	 * @return a valid Waypoint object with two segments countryname
+	 */
 
 public Waypoint(String country, String country2, String ICAO, String WPT_id, String latitude, String longitude){
 	this.country=(country.concat(",").concat(country2));
@@ -20,6 +31,16 @@ public Waypoint(String country, String country2, String ICAO, String WPT_id, Str
 	this.longitude=longitude;
 	this.longxlati=latitude.concat(longitude);
 }
+
+/*
+ * Constructor for waypoints which country name is made of one part (for example: Russia)
+ * @param country - String with first (and only) part of country name
+ * @param ICAO - Operators code String
+ * @param WPT_id Particular Waypoint id, that is unique (can not be repeated in whole continent)
+ * @param latitude - String with latitude data
+ * @param longitude - String with longitude data
+ * @return a valid Waypoint object with two segments countryname
+ */
 
 public Waypoint(String country, String ICAO, String WPT_id, String latitude, String longitude){
 	this.country=country;
